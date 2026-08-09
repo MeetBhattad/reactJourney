5ae6b337648df5da34c3b43ec2a844b6a82aba45
MeetBhattad <meetbhattad@gmail.com>
2026-08-10 01:49:27 +0530

    Implemented useState Hook

--- 01/src/myntra.js
+++ 01/src/myntra.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import Card from "./components/card";
 import Header from "./components/header";
@@ -6,15 +6,32 @@ import Footer from "./components/footer";
 import arr from "./utils/dummy";
 
 function App() {
+  let [A, setA] = useState(arr);
+  function sortPrice() {
+    A.sort((a, b) => a.price - b.price);
+    setA([...A]);
+  }
+
+  function above599() {
+    const B = arr.filter((value) => value.price > 599);
+    setA(B);
+  }
   return (
     <>
       <Header />
+      <button onClick={sortPrice}>Sort by Price</button>
+      <button onClick={above599}>Items above 599</button>
       <div
         className="middle"
         style={{ display: "flex", gap: "10px", flexWrap: "wrap" }}
       >
-        {arr.map((value, index) => (
-          <Card key={index} cloth={value.cloth} offer={value.offer} />
+        {A.map((value, index) => (
+          <Card
+            key={index}
+            cloth={value.cloth}
+            offer={value.offer}
+            price={value.price}
+          />
         ))}
       </div>
 
